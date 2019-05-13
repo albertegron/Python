66dbb2c1a008723d3e3ddb5eca210c02bfb23655
Albert Egron <47090048+albertegron@users.noreply.github.com>
2019-05-13 18:46:00 -0400

    Add files via upload

---
+++ ELSE and IF.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+people = 30
+cars = 40 
+buses = 15
+
+
+if cars > people:
+	print ("We should take the cars.")
+elif cars < people: 
+	print ("We should not take the cars.")
+else:
+	print ("We can't decide.")
+	
+if buses > cars:
+	print ("That's too many buses.")
+elif buses < cars:
+	print("Maybe we could take the buses.")
+else:
+	print ("We still can't decide.")
+	
+if people > buses:
+	print ("Alright, let's just take te buses.")
+else:
+	print ("Fine, let's stay home then.")
+
+
+# In[ ]:
+
+
+
+
