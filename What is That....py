358b4f685e7e20a21bc71bdb5c5b58396b957005
Albert Egron <47090048+albertegron@users.noreply.github.com>
2019-05-13 18:16:58 -0400

    Add files via upload

---
+++ What is That....py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Positioning the backslash at the beginning moves the sentance one tab forward
+tabby_cat = "\tI'm tabbed in."
+# Positioning the backslash at the middle of the sentance removes one letter after backslash and moves 
+# the rest of the sentance into new line  
+persian_cat = "I'm split\non a line."
+# Positioningthe two backslashes between a word shows only one backslash
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
+
+
+# In[ ]:
+
+
+
+
