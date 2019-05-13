3ab4d1784276c157694a845a603debe548f94499
Albert Egron <47090048+albertegron@users.noreply.github.com>
2019-05-13 18:43:37 -0400

    Add files via upload

---
+++ What IF.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+	print ("Too many cats! The world is doomed!")
+	
+if people > cats:
+	print ("Not many cats! The world is saved!")
+	
+if people < dogs:
+	print ("The world is drooled on!")
+	
+if people > dogs:
+	print ("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+	print ("People are greater than or equal to dogs.")
+	
+if people <= dogs:
+	("People are less than or equal to dogs.")
+	
+	
+if people == dogs:
+	print ("People are dogs.")
+	
+
+
+# In[ ]:
+
+
+
+
